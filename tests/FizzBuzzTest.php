9fde389bdfaabf102bb0140ad83b6ef32caa11e7
Samuel Otterman <samuelotterman@futurimedia.com>
2019-03-12 09:32:10 -0400

    test for FizzBuzz

---
+++ tests/FizzBuzzTest.php
@@ -0,0 +1,11 @@
+<?php 
+use Kata\FizzBuzz;
+class FizzBuzzTest extends \Codeception\Test\Unit
+{
+    public function testSomeFeature()
+    {
+        $fizzBuzz = new FizzBuzz();
+        $expected = array(1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz");
+        $this->assertEquals($fizzBuzz->getSequence(15), $expected);
+    }
+}
