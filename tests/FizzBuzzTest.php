fd1fe99c6c18f99477699711667cfb45b7619327
Samuel Otterman <samuelotterman@futurimedia.com>
2019-03-18 08:56:05 -0400

    fix function name for test

--- tests/FizzBuzzTest.php
+++ tests/FizzBuzzTest.php
@@ -2,10 +2,11 @@
 use Kata\FizzBuzz;
 class FizzBuzzTest extends \Codeception\Test\Unit
 {
-    public function testSomeFeature()
+    public function testFizzBuzz()
     {
         $fizzBuzz = new FizzBuzz();
         $expected = array(1, 2, "fizz", 4, "buzz", "fizz", 7, 8, "fizz", "buzz", 11, "fizz", 13, 14, "fizzbuzz");
         $this->assertEquals($fizzBuzz->getSequence(15), $expected);
     }
 }
+
